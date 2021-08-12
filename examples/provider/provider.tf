16239ac99a64c0bbce26793a655bd90dbe201420
Leo Przybylski <leo@cloudposse.com>
2021-08-12 06:29:41 -0700

    feat(provider): Adding export client config example to start draft pr

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -15,3 +15,8 @@ provider "awsutils" {
 # Delete default VPC
 resource "awsutils_default_vpc_deletion" "example" {
 }
+
+# Export default VPN client config
+resource "awsutils_export_client_config" "default" {
+    client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.default.id
+}

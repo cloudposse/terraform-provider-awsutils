2789abfd582ee961e112757d6de75072bea2ca68
Leo Przybylski <leo@cloudposse.com>
2021-09-13 22:37:24 -0700

    fix(provider): Adding comment for local development to example

--- examples/datasources/ec2_client_vpn_export_client_config_json/data-source.tf
+++ examples/datasources/ec2_client_vpn_export_client_config_json/data-source.tf
@@ -2,6 +2,10 @@ terraform {
   required_providers {
     awsutils = {
       source = "cloudposse/awsutils"
+      # For local development,
+      # install the provider on local computer by running `make install` from the root of the repo,
+      # and uncomment the version below
+      # version = "9999.99.99"
     }
   }
 }
